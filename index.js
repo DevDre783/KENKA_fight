66a401441f0b4799c3e02cbdd85bff0b0638526d
Andres Soca <lumen.development01@gmail.com>
2022-06-15 18:42:18 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector("canvas");
+const canv = canvas.getContext("2d");
+
+// display
+canvas.width = 1750;
+canvas.height = 1100;
+
+//interaction
+canv.fillRect(0, 0, canvas.width, canvas.height)
